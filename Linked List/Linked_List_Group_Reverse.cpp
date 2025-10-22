d4542e59a4cb47c853b266547139267cdd687f0b
Sk Anisuddin Ahmed <skanisuddinahmed@gmail.com>
2025-10-23 02:49:29 +0530

    updated code- previous code only executable on GFG platform

--- Linked List/Linked_List_Group_Reverse.cpp
+++ Linked List/Linked_List_Group_Reverse.cpp
@@ -1,43 +1,62 @@
 class Solution {
   public:
-    Node *reverseFourLink(Node* head, Node* prev, int k)
-    {
-        Node* Head = head;
-        Node* previous = nullptr;
-        Node* current = head;
-        
-        while (head && k--)
-        {
-            head = head->next;
-            current->next = previous;
-            previous = current;
-            current = head;
-        }
-        Head->next = head;
-        if (prev)
-            prev->next = previous;
-        return previous;
-    }
-  
-    Node *reverseKGroup(Node *head, int k) 
-    {
-        Node* temp = nullptr;
-        Node* prevLink = nullptr;
-        
-        while (head)
-        {
-            if (temp == nullptr)
-            {
-                temp = reverseFourLink(head, prevLink, k);
-            }
-            else
-            {
-                reverseFourLink(head, prevLink, k);
-            }
-            prevLink = head;
-            head = head->next;
-        }
-        
-        return temp;
-    }
+    //Helper function to reverse Kth times and link previous ends
+      ListNode *reverseLink(ListNode* head, ListNode* prev, int k)
+      {
+          ListNode* previous = nullptr;
+          ListNode* curr = head;        
+          while (head && k--)
+          {
+              head = head->next;
+              curr->next = previous;
+              previous = curr;
+              curr = head;
+          }        
+          if (prev)
+              prev->next = previous;
+          return previous;
+      }
+      ListNode* reverseKGroup(ListNode* head, int k) 
+      {
+          ListNode* previous = nullptr;
+          ListNode* returnHead = nullptr;
+          while(head)
+          {
+              int i = 1;
+              ListNode* temp = head;
+              for (; i < k; i++)
+              {
+                  if (!temp) break;
+                  temp = temp->next;
+              }
+              if (!temp)
+              {
+                  if (returnHead)
+                  {
+                      reverseLink(head, previous, i);
+                  }
+                  else
+                  {
+                      returnHead = reverseLink(head, previous, i);
+                  }
+                  previous = head;
+                  head = temp;
+              }
+              else
+              {
+                  temp = temp->next; //Reach next Kth first node
+                  if (returnHead)
+                  {
+                      reverseLink(head, previous, k);
+                  }
+                  else
+                  {
+                      returnHead = reverseLink(head, previous, k);
+                  }
+                  previous = head;
+                  head = temp;
+              }
+          }
+          return returnHead;
+      }
 };
