195bf6ef9780c7c7c394e4c425355c183f872d82
Sk Anisuddin Ahmed <skanisuddinahmed@gmail.com>
2025-10-23 02:41:27 +0530

    Added New DSA Solution - Reverse Nodes in K-Group

---
+++ Linked List/Reverse_Nodes_in_K_Group.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    //Helper function to reverse Kth times and link previous ends
+    ListNode *reverseLink(ListNode* head, ListNode* prev, int k)
+    {
+        ListNode* previous = nullptr;
+        ListNode* curr = head;        
+        while (head && k--)
+        {
+            head = head->next;
+            curr->next = previous;
+            previous = curr;
+            curr = head;
+        }        
+        if (prev)
+            prev->next = previous;
+        return previous;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) 
+    {
+        ListNode* previous = nullptr;
+        ListNode* returnHead = nullptr;
+        while(head)
+        {
+            int i = 1;
+            ListNode* temp = head;
+            for (; i < k; i++)
+            {
+                if (!temp) break;
+                temp = temp->next;
+            }
+            if (!temp)
+            {
+                //Don't Reverse Just Link - Remaining List
+                if (previous)
+                    previous->next = head;
+                break;
+            }
+            else
+            {
+                temp = temp->next; //Reach next Kth first node
+                if (returnHead)
+                {
+                    reverseLink(head, previous, k);
+                }
+                else
+                {
+                    returnHead = reverseLink(head, previous, k);
+                }
+                previous = head;
+                head = temp;
+            }
+        }
+        return returnHead;
+    }
+};
